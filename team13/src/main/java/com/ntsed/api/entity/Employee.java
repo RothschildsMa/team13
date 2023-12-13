b20e68927112d6338d4b822f6e56d72287d9d2d2
Ari <2449639117@qq.com>
2023-12-13 13:53:32 +0900

    实体类

---
+++ team13/src/main/java/com/ntsed/api/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.ntsed.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+	private String employeeId;
+    private String employeeName;
+    private String password;
+    private String deptId;
+    private Integer delFlg;
+    private String createDate;
+    private String createUser;
+    private String updateDate;
+    private String updateUser;
+    private String gender;
+    private String age;
+    private String startDay;
+    private String emailAddress;
+    private String phoneNumber;
+
+}
